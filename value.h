d09a5c5bf6460dd8510fa308aabe5d5d64233c1b
Fedjmike <sam.nipps@gmail.com>
2015-07-12 21:50:46 +0100

    Embettered a comment

--- value.h
+++ value.h
@@ -6,14 +6,14 @@
 /*Opaque, use interface below*/
 typedef struct value value;
 
-/*valueCreateXXX allocate objects with a garbage collector!
+/*The value creators allocate objects with a garbage collector!
 
   These objects are only kept alive by references stored in:
     - The stack and data segments (variables and parameters)
     - Other GC allocated objects
 
   Therefore if you want a value to be owned by a manually managed object,
-  allocate that one with GC_MALLOC_UNCOLLECTABLE and free with GC_FREE.
+  allocate the owner with GC_MALLOC_UNCOLLECTABLE and free with GC_FREE.
   This is just a normal allocation that gets scanned for GC object
   references.*/
 
