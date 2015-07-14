d929a0ce3f3f31ae154ae33dc543a788cb68b55a
Fedjmike <sam.nipps@gmail.com>
2015-07-15 00:12:10 +0100

    Compiler driver

--- sh.c
+++ sh.c
@@ -15,6 +15,8 @@
 #include "value.h"
 #include "runner.h"
 
+/*==== Builtins ====*/
+
 void addBuiltin (sym* global, const char* name, type* dt) {
     symAdd(global, name)->dt = dt;
 }
@@ -23,35 +25,77 @@ void addBuiltins (typeSys* ts, sym* global) {
     addBuiltin(global, "size", typeFn(ts, typeFile(ts), typeInteger(ts)));
 }
 
-int main (int argc, char** argv) {
-    (void) argc, (void) argv;
+/*==== Compiler ====*/
 
-    GC_INIT();
+typedef struct compilerCtx {
+    typeSys ts;
+    sym* global;
+} compilerCtx;
 
-    typeSys ts = typesInit();
-
-    sym* global = symInit();
-    addBuiltins(&ts, global);
-
-    char* str = "sh.c size";
-    //char* str = "*.cpp wc | sort";
-
+ast* compile (compilerCtx* ctx, const char* str) {
+    /*Turn the string into an AST*/
     lexerCtx lexer = lexerInit(str);
-    ast* tree = parse(global, &lexer);
+    ast* tree = parse(ctx->global, &lexer);
     lexerDestroy(&lexer);
 
-    analyze(&ts, tree);
+    /*Add types and other semantic information*/
+    analyze(&ctx->ts, tree);
     printAST(tree);
 
+    return tree;
+}
+
+compilerCtx compilerInit (void) {
+    return (compilerCtx) {
+        .ts = typesInit(),
+        .global = symInit()
+    };
+}
+
+compilerCtx* compilerFree (compilerCtx* ctx) {
+    symEnd(ctx->global);
+    typesFree(&ctx->ts);
+    return ctx;
+}
+
+/*==== Gosh ====*/
+
+void gosh (compilerCtx* ctx, const char* str) {
+    ast* tree = compile(ctx, str);
+
+    /*Run the AST*/
     envCtx env = {};
     value* result = run(&env, tree);
 
+    /*Print the value and type*/
     valuePrint(result);
     printf(" :: %s\n", typeGetStr(tree->dt));
 
     astDestroy(tree);
+}
 
-    symEnd(global);
+/*==== ====*/
 
-    typesFree(&ts);
+const char* const samples[] = {
+    "sh.c size",
+    "[sh.c, parser.c]",
+    "[sh.c, parser.c] | size"
+};
+
+int main (int argc, char** argv) {
+    GC_INIT();
+
+    compilerCtx ctx = compilerInit();
+    addBuiltins(&ctx.ts, ctx.global);
+
+    if (argc == 1)
+        gosh(&ctx, samples[2]);
+
+    else if (argc == 2)
+        gosh(&ctx, argv[1]);
+
+    else
+        printf("Unknown arguments.\n");
+
+    compilerFree(&ctx);
 }
