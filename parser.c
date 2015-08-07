846e530aee33c8382ed2c414be249bf869dabdef
Fedjmike <sam.nipps@gmail.com>
2015-08-07 06:04:32 +0100

    Reversed the function impls for a more logical ordering which removes the need for forward decls

--- parser.c
+++ parser.c
@@ -8,155 +8,39 @@
 #include "sym.h"
 #include "ast.h"
 
-static ast* parserS (parserCtx* ctx);
-
 static ast* parserExpr (parserCtx* ctx);
-static ast* parserBOP (parserCtx* ctx, int level);
-static ast* parserFnApp (parserCtx* ctx);
-static ast* parserAtom (parserCtx* ctx);
-static ast* parserPath (parserCtx* ctx);
-
-parserResult parse (sym* global, lexerCtx* lexer) {
-    parserCtx ctx = parserInit(global, lexer);
-    ast* tree = parserS(&ctx);
-
-    parserResult result = {
-        .tree = tree,
-        .errors = ctx.errors
-    };
-
-    parserFree(&ctx);
-
-    return result;
-}
-
-static ast* parserS (parserCtx* ctx) {
-    ast* node = parserExpr(ctx);
-
-    if (see_kind(ctx, tokenEOF))
-        accept(ctx);
-
-    else
-        error(ctx)("Expected end of text, found '%s'\n", ctx->current.buffer);
-
-    return node;
-}
-
-static ast* parserExpr (parserCtx* ctx) {
-    return parserBOP(ctx, 0);
-}
 
 /**
- * BOP = Pipe
- * Pipe    = Logical  [{ "|" | "|>" Logical }]
- * Logical = Equality [{ "&&" | "||" Equality }]
- * Equality = Sum     [{ "==" | "!=" | "<" | "<=" | ">" | ">=" Sum }]
- * Sum     = Product  [{ "+" | "-" Product }]
- * Product = Exit     [{ "*" | "/" | "%" Exit }]
- * Exit = FnApp
+ * Path = <PathLit> | <GlobLit>
  *
- * The grammar is ambiguous, but these operators are left associative,
- * which is to say:
- *    x op y op z == (x op y) op z
+ * A glob literal is a path with a wildcard somewhere in it.
  */
-static ast* parserBOP (parserCtx* ctx, int level) {
-    /* (1) Operator precedence parsing!
-      As all the productions above are essentially the same, with
-      different operators, we can parse them with the same function.
+static ast* parserPath (parserCtx* ctx) {
+    bool modifier = false,
+         glob = false;
 
-      Read the comments in numbered order, this was (1)*/
+    const char* str = ctx->current.buffer;
 
-    /* (5) Finally, once we reach the top level we escape the recursion
-           into... */
-    if (level == 5)
-        return parserFnApp(ctx);
-
-    /* (2) The left hand side is the production one level up*/
-    ast* node = parserBOP(ctx, level+1);
-    opKind op;
-
-    /* (3) Accept operators associated with this level, and store
-           which kind is found*/
-    while (  level == 0
-             ? (op =   try_match(ctx, "|") ? opPipe
-                     : try_match(ctx, "|>") ? opWrite : opNull)
-           : level == 1
-             ? (op =   try_match(ctx, "&&") ? opLogicalAnd
-                     : try_match(ctx, "||") ? opLogicalOr : opNull)
-           : level == 2
-             ? (op =   try_match(ctx, "==") ? opEqual
-                     : try_match(ctx, "!=") ? opNotEqual
-                     : try_match(ctx, "<") ? opLess
-                     : try_match(ctx, "<=") ? opLessEqual
-                     : try_match(ctx, ">") ? opGreater
-                     : try_match(ctx, ">=") ? opGreaterEqual : opNull)
-           : level == 3
-             ? (op =   try_match(ctx, "+") ? opAdd
-                     : try_match(ctx, "-") ? opSubtract : opNull)
-           : level == 4
-             ? (op =   try_match(ctx, "*") ? opMultiply
-                     : try_match(ctx, "/") ? opDivide
-                     : try_match(ctx, "%") ? opModulo : opNull)
-           : (op = opNull)) {
-        /* (4) Bundle it up with an RHS, also the level up*/
-        ast* rhs = parserBOP(ctx, level+1);
-        node = astCreateBOP(node, rhs, op);
+    /*Path modifier*/
+    if (*str == '/') {
+        modifier = true;
+        str++;
     }
 
-    return node;
-}
+    /*Search the path segments looking for glob operators
+      (yes, could just strchr directly but in the future this fn
+       will use the segments)*/
 
-static bool waiting_for_delim (parserCtx* ctx) {
-    return waiting(ctx) && !see(ctx, "|") && !see(ctx, ",") && !see(ctx, ")") && !see(ctx, "]");
-}
+    char* segments = pathGetSegments(str, malloc);
 
-/**
- * FnApp = { Atom | ( "`" Atom "`" ) }
- *
- * A series of at least one expr-atom. If multiple, then the last is a
- * function to which the others are applied. Instead, one of them may
- * be explicitly marked in backticks as the function.
- */
-static ast* parserFnApp (parserCtx* ctx) {
-    /*Filled iff there is a backtick function*/
-    ast* fn = 0;
-
-    vector(ast*) nodes = vectorInit(3, malloc);
-
-    /*Require at least one expr*/
-    if (!see(ctx, "`"))
-        vectorPush(&nodes, parserAtom(ctx));
-
-    while (waiting_for_delim(ctx)) {
-        if (try_match(ctx, "`")) {
-            if (fn) {
-                error(ctx)("Multiple explicit functions in backticks: '%s'\n", ctx->current.buffer);
-                vectorPush(&nodes, fn);
-            }
-
-            fn = parserAtom(ctx);
-            match(ctx, "`");
-
-        } else
-            vectorPush(&nodes, parserAtom(ctx));
+    for (char* segment = segments; *segment; segment += strlen(segment)+1) {
+        if (strchr(segment, '*'))
+            glob = true;
     }
 
-    if (nodes.length == 1) {
-        ast* node = vectorPop(&nodes);
-        vectorFree(&nodes);
-        return node;
+    free(segments);
 
-    } else if (nodes.length == 0 && fn) {
-        error(ctx)("No arguments provided to backtick-marked function");
-        return fn;
-
-    } else {
-        /*If not explicitly marked in backticks, the last expr was the fn*/
-        if (!fn)
-            fn = vectorPop(&nodes);
-
-        return astCreateFnApp(nodes, fn);
-    }
+    return (glob ? astCreateGlobLit : astCreateFileLit)(ctx->current.buffer);
 }
 
 static bool isPathToken (const char* str) {
@@ -219,35 +103,145 @@ static ast* parserAtom (parserCtx* ctx) {
     return node;
 }
 
+static bool waiting_for_delim (parserCtx* ctx) {
+    return waiting(ctx) && !see(ctx, "|") && !see(ctx, ",") && !see(ctx, ")") && !see(ctx, "]");
+}
+
 /**
- * Path = <PathLit> | <GlobLit>
+ * FnApp = { Atom | ( "`" Atom "`" ) }
  *
- * A glob literal is a path with a wildcard somewhere in it.
+ * A series of at least one expr-atom. If multiple, then the last is a
+ * function to which the others are applied. Instead, one of them may
+ * be explicitly marked in backticks as the function.
  */
-static ast* parserPath (parserCtx* ctx) {
-    bool modifier = false,
-         glob = false;
+static ast* parserFnApp (parserCtx* ctx) {
+    /*Filled iff there is a backtick function*/
+    ast* fn = 0;
 
-    const char* str = ctx->current.buffer;
+    vector(ast*) nodes = vectorInit(3, malloc);
 
-    /*Path modifier*/
-    if (*str == '/') {
-        modifier = true;
-        str++;
+    /*Require at least one expr*/
+    if (!see(ctx, "`"))
+        vectorPush(&nodes, parserAtom(ctx));
+
+    while (waiting_for_delim(ctx)) {
+        if (try_match(ctx, "`")) {
+            if (fn) {
+                error(ctx)("Multiple explicit functions in backticks: '%s'\n", ctx->current.buffer);
+                vectorPush(&nodes, fn);
+            }
+
+            fn = parserAtom(ctx);
+            match(ctx, "`");
+
+        } else
+            vectorPush(&nodes, parserAtom(ctx));
     }
 
-    /*Search the path segments looking for glob operators
-      (yes, could just strchr directly but in the future this fn
-       will use the segments)*/
+    if (nodes.length == 1) {
+        ast* node = vectorPop(&nodes);
+        vectorFree(&nodes);
+        return node;
 
-    char* segments = pathGetSegments(str, malloc);
+    } else if (nodes.length == 0 && fn) {
+        error(ctx)("No arguments provided to backtick-marked function");
+        return fn;
 
-    for (char* segment = segments; *segment; segment += strlen(segment)+1) {
-        if (strchr(segment, '*'))
-            glob = true;
+    } else {
+        /*If not explicitly marked in backticks, the last expr was the fn*/
+        if (!fn)
+            fn = vectorPop(&nodes);
+
+        return astCreateFnApp(nodes, fn);
+    }
+}
+
+/**
+ * BOP = Pipe
+ * Pipe    = Logical  [{ "|" | "|>" Logical }]
+ * Logical = Equality [{ "&&" | "||" Equality }]
+ * Equality = Sum     [{ "==" | "!=" | "<" | "<=" | ">" | ">=" Sum }]
+ * Sum     = Product  [{ "+" | "-" Product }]
+ * Product = Exit     [{ "*" | "/" | "%" Exit }]
+ * Exit = FnApp
+ *
+ * The grammar is ambiguous, but these operators are left associative,
+ * which is to say:
+ *    x op y op z == (x op y) op z
+ */
+static ast* parserBOP (parserCtx* ctx, int level) {
+    /* (1) Operator precedence parsing!
+      As all the productions above are essentially the same, with
+      different operators, we can parse them with the same function.
+
+      Read the comments in numbered order, this was (1)*/
+
+    /* (5) Finally, once we reach the top level we escape the recursion
+           into... */
+    if (level == 5)
+        return parserFnApp(ctx);
+
+    /* (2) The left hand side is the production one level up*/
+    ast* node = parserBOP(ctx, level+1);
+    opKind op;
+
+    /* (3) Accept operators associated with this level, and store
+           which kind is found*/
+    while (  level == 0
+             ? (op =   try_match(ctx, "|") ? opPipe
+                     : try_match(ctx, "|>") ? opWrite : opNull)
+           : level == 1
+             ? (op =   try_match(ctx, "&&") ? opLogicalAnd
+                     : try_match(ctx, "||") ? opLogicalOr : opNull)
+           : level == 2
+             ? (op =   try_match(ctx, "==") ? opEqual
+                     : try_match(ctx, "!=") ? opNotEqual
+                     : try_match(ctx, "<") ? opLess
+                     : try_match(ctx, "<=") ? opLessEqual
+                     : try_match(ctx, ">") ? opGreater
+                     : try_match(ctx, ">=") ? opGreaterEqual : opNull)
+           : level == 3
+             ? (op =   try_match(ctx, "+") ? opAdd
+                     : try_match(ctx, "-") ? opSubtract : opNull)
+           : level == 4
+             ? (op =   try_match(ctx, "*") ? opMultiply
+                     : try_match(ctx, "/") ? opDivide
+                     : try_match(ctx, "%") ? opModulo : opNull)
+           : (op = opNull)) {
+        /* (4) Bundle it up with an RHS, also the level up*/
+        ast* rhs = parserBOP(ctx, level+1);
+        node = astCreateBOP(node, rhs, op);
     }
 
-    free(segments);
+    return node;
+}
 
-    return (glob ? astCreateGlobLit : astCreateFileLit)(ctx->current.buffer);
+static ast* parserExpr (parserCtx* ctx) {
+    return parserBOP(ctx, 0);
+}
+
+static ast* parserS (parserCtx* ctx) {
+    ast* node = parserExpr(ctx);
+
+    if (see_kind(ctx, tokenEOF))
+        accept(ctx);
+
+    else
+        error(ctx)("Expected end of text, found '%s'\n", ctx->current.buffer);
+
+    return node;
+}
+
+parserResult parse (sym* global, lexerCtx* lexer) {
+    parserCtx ctx = parserInit(global, lexer);
+    ast* tree = parserS(&ctx);
+
+    parserResult result = {
+        .tree = tree,
+        .errors = ctx.errors
+    };
+
+    parserFree(&ctx);
+
+    return result;
 }
